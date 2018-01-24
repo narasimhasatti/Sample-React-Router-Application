6428006ef03b42e34a560fcb1765baa33653edd2
narasimhasatti <narasimhasatti@users.noreply.github.com>
2018-01-24 14:24:19 -0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,9 @@ import {Route ,IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-
 export default(
 <Route path="/" component={App}>
-    <IndexRoute component ={HomePage}/>
-    <Route path="about" component={AboutPage}/>
+   <IndexRoute component ={HomePage}/>
+   <Route path="about" component={AboutPage}/>
 </Route>
 );
