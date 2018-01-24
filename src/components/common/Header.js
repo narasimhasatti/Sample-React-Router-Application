a87f4eac81d3fbcdbeed64d663078e3b225af5aa
narasimhasatti <narasimhasatti@users.noreply.github.com>
2018-01-24 14:33:11 -0800

    Update Header.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,12 @@
 import React, {PropTypes} from 'react';
 import {Link , IndexLink} from 'react-router';
-
 const Header = () => {
-    return(
-        <nav>
-            <IndexLink to="/" activeClassName="active">Home</IndexLink>
-            {" | "}
-             <IndexLink to="/about" activeClassName="active">About</IndexLink>
-            </nav>
-    );
+return(
+<nav>
+   <IndexLink to="/" activeClassName="active">Home</IndexLink>
+   {" | "}
+   <IndexLink to="/about" activeClassName="active">About</IndexLink>
+</nav>
+);
 };
-
 export default Header;
