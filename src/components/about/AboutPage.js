a19e465e5b4c473c6cd29446015d0074f851fd66
narasimhasatti <narasimhasatti@users.noreply.github.com>
2018-01-24 14:32:22 -0800

    Update AboutPage.js

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,18 +1,12 @@
 import React from 'react';
-
 class AboutPage extends React.Component{
-    render(){
-
-        return(
-            <div className="jumbotron">
-            <h1>Application Home Page</h1>
-            <p> This is an appplication About us page</p>
-           </div>
-
-        );
-    }
-
-
+render(){
+return(
+<div className="jumbotron">
+   <h1>Application Home Page</h1>
+   <p> This is an appplication About us page</p>
+</div>
+);
 }
-
+}
 export default AboutPage;
