db8c8119a2244fa80ede144313c1b86ddc894558
narasimhasatti <narasimhasatti@users.noreply.github.com>
2018-01-24 14:33:57 -0800

    Update HomePage.js

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router';
 class HomePage extends React.Component{
-    render(){
-
-        return(
-            <div className="jumbotron">
-            <h1>Application Home Page</h1>
-            <p> This is an appplication home page</p>
-            <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
-            </div>
-
-        );
-    }
-
-
+render(){
+return(
+<div className="jumbotron">
+   <h1>Application Home Page</h1>
+   <p> This is an appplication home page</p>
+   <Link to="about" className="btn btn-primary btn-lg">
+   Learn More</Link>
+</div>
+);
 }
-
+}
 export default HomePage;
